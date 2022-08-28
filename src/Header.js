c07380179e68563566290757a080949b808d2c60
Manish Veerani <manishveerani0208@gmail.com>
2022-08-28 20:47:59 +0530

    setup header-navbar

---
+++ src/Header.js
@@ -0,0 +1,43 @@
+import React from "react"
+import "./Header.css";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className='header_logo'>
+              <StorefrontIcon className='header_logo_image' fontSize='large'/>
+              <h2 className='header_logo_title'>aLite</h2>
+
+            </div>
+
+            <div className='header_search'>
+              <input type='text' className='header_search_input' />
+              <SearchIcon className='header_search_icon'/>
+            </div>
+
+            <div className='header_nav'>
+              <div className='nav_item'>
+                <span className='nav_item_lineOne'>Hello Guest</span>
+                <span className='nav_item_lineTwo'>Sign In</span>
+              </div>
+              <div className='nav_item'>
+                <span className='nav_item_lineOne'>Your</span>
+                <span className='nav_item_lineTwo'>Shop</span>
+              </div>
+              <div className='nav_item'>
+                <ShoppingBasketIcon className="nav_itemBasket" />
+                <span className='nav_basketCount'>0</span>
+              </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
