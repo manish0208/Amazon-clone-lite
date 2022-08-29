7240a9acccba7ec1fb3c2dca15d21ef15729d7c7
Manish Veerani <manishveerani0208@gmail.com>
2022-08-30 00:04:15 +0530

    setup Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
+import React from 'react';
 import './App.css';
-
 import Header from "./Header.js";
 import Home from "./Home.js";
+import Checkout from "./Checkout.js";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+
+        <Routes>
+          <Route path="/checkout" element={<><Header/><Checkout/></>}></Route>
+          <Route path='/' element={<><Header/><Home/></>}></Route> 
+        </Routes>
+ 
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,23 @@
-import React from "react"
+import React from "react";
 import "./Header.css";
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 import SearchIcon from '@mui/icons-material/Search';
+import { Link } from "react-router-dom";
 
 
 
 
 
-const Header = () => {
+function Header() {
     return(
         <div className="header">
+          <Link to="/" style={{ textDecoration: "none" }}>
             <div className='header_logo'>
               <StorefrontIcon className='header_logo_image' fontSize='large'/>
               <h2 className='header_logo_title'>aLite</h2>
-
             </div>
-
+          </Link>
             <div className='header_search'>
               <input type='text' className='header_search_input' />
               <SearchIcon className='header_search_icon'/>
@@ -31,10 +32,13 @@ const Header = () => {
                 <span className='nav_item_lineOne'>Your</span>
                 <span className='nav_item_lineTwo'>Shop</span>
               </div>
-              <div className="nav_itemBasket">
-                <ShoppingBasketIcon  />
-                <span className='nav_basketCount'>0</span>
-              </div>
+              <Link to="/checkout" style={{ textDecoration: "none" }}>
+                <div className="nav_itemBasket">
+                  <ShoppingBasketIcon  />
+                  <span className='nav_basketCount'>0</span>
+                </div>
+              </Link>
+              
             </div>
         </div>
     )
