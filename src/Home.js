9956db1fa0e71a12199600b4cf9f6e78ed178b6f
Manish Veerani <manishveerani0208@gmail.com>
2022-08-29 17:27:26 +0530

    complete product segment

--- src/Home.js
+++ src/Home.js
@@ -10,18 +10,54 @@ const Home = () => {
                 <img src="https://www.x-cart.com/wp-content/uploads/2019/01/ecommerce-768x278.jpg" alt="" className="home_image" />
 
                 <div className="home_row">
-                    <Product />
-                    <Product />
+                    <Product 
+                        id="123321"
+                        title="Betula Women's Tote Bag | Ladies Purse Handbag" 
+                        price={15.56}
+                        rating={4}
+                        image="https://m.media-amazon.com/images/I/717BChXEX7S._AC_UL480_FMwebp_QL65_.jpg"
+                    />
+                    <Product 
+                         id="123500"
+                         title="Apple iPhone 13 (128GB) - Blue" 
+                         price={799}
+                         rating={5}
+                         image="https://m.media-amazon.com/images/I/71xb2xkN5qL._SX679_.jpg"
+                    />
                 </div>
 
                 <div className="home_row">
-                    <Product />
-                    <Product />
-                    <Product />
+                    <Product 
+                         id="123400"
+                         title="Neo Iv Analog Black Dial Men's Watch-NL1805NM01/NP1805NM01" 
+                         price={299}
+                         rating={5}
+                         image="https://m.media-amazon.com/images/I/71xQq6cAhGL._AC_UL480_FMwebp_QL65_.jpg"
+                    />
+                    <Product 
+                         id="123789"
+                         title="ASUS TUF Gaming F15 (2022), 15.6-inch (39.62 cms) FHD 300Hz, Intel Core i7-12700H 12th Gen, RTX 3060 6GB Graphics, Gaming Laptop (16GB/1TB SSD/Windows 11/Office 2021/Gray/2.2 Kg)" 
+                         price={499}
+                         rating={4}
+                         image="https://m.media-amazon.com/images/I/71NxKpg1NIL._SX679_.jpg"
+                    />
+                    <Product 
+                         id="123000"
+                         title="Refulgix LED Wine Bottle Cork Lights Copper Wire String Lights, 2M Battery Powered Fairy Lights Bottle " 
+                         price={89}
+                         rating={5}
+                         image="https://m.media-amazon.com/images/I/71zxmbEruGL._SX522_.jpg"
+                    />
                 </div>
 
                 <div className="home_row">
-                    <Product />
+                    <Product 
+                         id="123456"
+                         title="Puma Men's Dazzler Sneaker" 
+                         price={99}
+                         rating={4}
+                         image="https://m.media-amazon.com/images/I/713UmakQhoL._AC_UL480_FMwebp_QL65_.jpg"
+                    />
                 </div>
             </div>
         </div>
--- src/Product.js
+++ src/Product.js
@@ -2,26 +2,26 @@ import React from "react";
 import "./Product.css";
 import StarIcon from '@mui/icons-material/Star';
 
-function Product() {
+function Product({id, title, image, price, rating}) {
     return(
         <div className="product">
             <div className="product_info">
-                <p>Betula Women's Tote Bag | Ladies Purse Handbag</p>
+                <p>{title}</p>
                 <p className="product_price">
                     <small>$</small>
-                    <strong>45</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="product_rating">
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
-                    <StarIcon />
+                    {Array(rating)
+                    .fill()
+                    .map((_,i) => (
+                        <p><StarIcon /></p>
+                    ))}
                 </div>
 
             </div>
             
-            <img src="https://m.media-amazon.com/images/I/717BChXEX7S._AC_UL480_FMwebp_QL65_.jpg" alt="product-image"/>
+            <img src={image} />
 
             <button>Add to Basket</button>
         </div>
